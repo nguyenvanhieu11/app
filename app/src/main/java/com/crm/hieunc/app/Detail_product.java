bfdc264bd45a0dda1fe7aba6c224065e0f2312de
DESKTOP-MTD5TMJ\Savvycom <vanhieumatmun1998@gmail.com>
2018-10-04 19:19:57 +0700

    detail product

---
+++ app/src/main/java/com/crm/hieunc/app/Detail_product.java
@@ -0,0 +1,59 @@
+package com.crm.hieunc.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TabHost;
+
+public class Detail_product extends AppCompatActivity {
+
+    ImageButton backActivity;
+    Toolbar toolbar;
+    TabHost tabhost;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_poduct);
+
+        anhXa();
+        backActivity = (ImageButton)findViewById(R.id.backActivity);
+        backActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Detail_product.this,SanPham.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void anhXa() {
+        tabhost = (TabHost)findViewById(R.id.tabHost);
+        tabhost.setup();
+
+        TabHost.TabSpec tab1 = tabhost.newTabSpec("Tab01");
+        tab1.setIndicator("",getResources().getDrawable(R.drawable.home));
+        tab1.setContent(R.id.tab1);
+        tabhost.addTab(tab1);
+
+        TabHost.TabSpec tab2 = tabhost.newTabSpec("Tab02");
+        tab2.setIndicator("",getResources().getDrawable(R.drawable.address));
+        tab2.setContent(R.id.tab2);
+        tabhost.addTab(tab2);
+
+        TabHost.TabSpec tab3 = tabhost.newTabSpec("Tab03");
+        tab3.setIndicator("",getResources().getDrawable(R.drawable.setting));
+        tab3.setContent(R.id.tab3);
+        tabhost.addTab(tab3);
+
+        TabHost.TabSpec tab4 = tabhost.newTabSpec("Tab04");
+        tab4.setIndicator("",getResources().getDrawable(R.drawable.search));
+        tab4.setContent(R.id.tab4);
+        tabhost.addTab(tab4);
+    }
+}
