23073a414d6adc31c9f08fcc2ed8a837dc8faaaf
DESKTOP-2LGBKPP\QuangTrung <HoangHaoNhien0110@hotmail.com>
2018-10-05 13:09:28 +0700

    hiển thị layout

--- app/src/main/java/com/crm/hieunc/app/HomePage.java
+++ app/src/main/java/com/crm/hieunc/app/HomePage.java
@@ -1,6 +1,5 @@
 package com.crm.hieunc.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
@@ -8,26 +7,19 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     Toolbar toolbar;
 
-    GridView gvImg;
-    ArrayList<HinhAnh> arrayImg;
-    HinhAnhAdapter adapter;
-    EditText edtsearch;
-    ImageButton imgbtnsearch;
+//    GridView gvImg;
+//    ArrayList<HinhAnh> arrayImg;
+//    HinhAnhAdapter adapter;
+//    EditText edtsearch;
+//    ImageButton imgbtnsearch;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +33,7 @@ public class HomePage extends AppCompatActivity {
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.home);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_hamberger);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
 
@@ -70,6 +62,9 @@ public class HomePage extends AppCompatActivity {
                                 break;
                             case R.id.login:
                                 getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new Login()).commit();
+                                break;
+                            default:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new TrangChu()).commit();
                                 break;
 
                         }
@@ -102,9 +97,9 @@ public class HomePage extends AppCompatActivity {
                 }
         );
 
-        khoitao();
-        adapter = new HinhAnhAdapter(this, R.layout.column_image, arrayImg);
-        gvImg.setAdapter(adapter);
+//        khoitao();
+//        adapter = new HinhAnhAdapter(this, R.layout.column_image, arrayImg);
+//        gvImg.setAdapter(adapter);
 
 
     }
@@ -120,46 +115,46 @@ public class HomePage extends AppCompatActivity {
     }
 
 
-    private void khoitao() {
-        gvImg = (GridView) findViewById(R.id.gridViewImg);
-        edtsearch = (EditText) findViewById(R.id.searchIndex);
-        imgbtnsearch = (ImageButton) findViewById(R.id.searchIndexbtn);
-
-        gvImg = (GridView) findViewById(R.id.gridViewImg);
-        arrayImg = new ArrayList<>();
-        arrayImg.add(new HinhAnh("dien tu", R.drawable.dientu));
-        arrayImg.add(new HinhAnh("me & be", R.drawable.me_be));
-        arrayImg.add(new HinhAnh("thu cung", R.drawable.thucung));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.anh1));
-        arrayImg.add(new HinhAnh("trang suc", R.drawable.anh2));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.anh3));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.anh4));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.anh5));
-        arrayImg.add(new HinhAnh("gia dung", R.drawable.giadung));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.anh6));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.nh7));
-        arrayImg.add(new HinhAnh("xe co", R.drawable.xeco));
-        eventSearch();
-        checkdanhmuc();
-    }
-
-    public void eventSearch() {
-        imgbtnsearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(HomePage.this, edtsearch.getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void checkdanhmuc() {
-        gvImg.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(HomePage.this, SanPham.class);
-                startActivity(intent);
-            }
-        });
-    }
+//    private void khoitao() {
+//        gvImg = (GridView) findViewById(R.id.gridViewImg);
+//        edtsearch = (EditText) findViewById(R.id.searchIndex);
+//        imgbtnsearch = (ImageButton) findViewById(R.id.searchIndexbtn);
+//
+//        gvImg = (GridView) findViewById(R.id.gridViewImg);
+//        arrayImg = new ArrayList<>();
+//        arrayImg.add(new HinhAnh("dien tu", R.drawable.dientu));
+//        arrayImg.add(new HinhAnh("me & be", R.drawable.me_be));
+//        arrayImg.add(new HinhAnh("thu cung", R.drawable.thucung));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.anh1));
+//        arrayImg.add(new HinhAnh("trang suc", R.drawable.anh2));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.anh3));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.anh4));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.anh5));
+//        arrayImg.add(new HinhAnh("gia dung", R.drawable.giadung));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.anh6));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.nh7));
+//        arrayImg.add(new HinhAnh("xe co", R.drawable.xeco));
+//        eventSearch();
+//        checkdanhmuc();
+//    }
+//
+//    public void eventSearch() {
+//        imgbtnsearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(HomePage.this, edtsearch.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+//
+//    private void checkdanhmuc() {
+//        gvImg.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(HomePage.this, SanPham.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 
 }
