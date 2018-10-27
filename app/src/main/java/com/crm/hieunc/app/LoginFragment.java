62ec0b63630485f6dab71ef21adf9814b86016d1
DESKTOP-MTD5TMJ\Savvycom <vanhieumatmun1998@gmail.com>
2018-10-27 22:59:18 +0700

    tạo giao diện màn lích sử hd

---
+++ app/src/main/java/com/crm/hieunc/app/LoginFragment.java
@@ -0,0 +1,23 @@
+package com.crm.hieunc.app;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.baiviet, container, false);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/crm/hieunc/app/SecoundFragment.java
+++ app/src/main/java/com/crm/hieunc/app/SecoundFragment.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class History_Search extends Fragment {
-
+public class SecoundFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.history_search, container, false);
+        View view = inflater.inflate(R.layout.timkiemgan, container, false);
+
 
         return view;
     }
