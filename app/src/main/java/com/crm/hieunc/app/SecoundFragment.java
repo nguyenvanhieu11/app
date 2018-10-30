93815f889e5a26bbccc1654bd0e51b9c63202dfd
DESKTOP-MTD5TMJ\Savvycom <vanhieumatmun1998@gmail.com>
2018-10-30 20:58:25 +0700

    fix conflic

--- app/src/main/java/com/crm/hieunc/app/SecoundFragment.java
+++ app/src/main/java/com/crm/hieunc/app/SecoundFragment.java
@@ -12,22 +12,8 @@ public class SecoundFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-<<<<<<< HEAD
-<<<<<<< HEAD:app/src/main/java/com/crm/hieunc/app/History_Search.java
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/hieucompany
-        View view = inflater.inflate(R.layout.history_search, container, false);
-=======
         View view = inflater.inflate(R.layout.timkiemgan, container, false);
 
->>>>>>> home:app/src/main/java/com/crm/hieunc/app/SecoundFragment.java
-
-=======
-        View view = inflater.inflate(R.layout.timkiemgan, container, false);
-
->>>>>>> 2fa113f23c210886b5f3b320f5fa1da1729b1487
         return view;
     }
 }
